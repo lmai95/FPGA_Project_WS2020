309c6fa8d6c8a9a169d129e23468a73e798de19b
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2020-12-16 16:59:32 +0100

    Update signal_processing_TESTER.vhd

--- signal_processing_TESTER.vhd
+++ signal_processing_TESTER.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_1164.all;
 entity signal_processing_TESTER is
 	port
 	(
+    --example
 		S : in std_logic;
 		R : in std_logic;
 
@@ -17,6 +18,7 @@ end entity signal_processing_TESTER;
 
 architecture sim of signal_processing_TESTER is
 begin
+  --example
 	S <= '0', '1' after 100ps, '0' after 200ps, '1' after 300ps;
 	R <= '0', '0' after 100ps, '1' after 200ps, '1' after 300ps;
 end architecture sim;
