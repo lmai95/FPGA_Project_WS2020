a531ee9197174778f60b55519d8465dba78ae87b
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2020-12-20 20:10:32 +0100

    Update signal_processing_TESTER.vhd

--- signal_processing_TESTER.vhd
+++ signal_processing_TESTER.vhd
@@ -19,6 +19,6 @@ end entity signal_processing_TESTER;
 architecture sim of signal_processing_TESTER is
 begin
   --example
-	S <= '0', '1' after 100ps, '0' after 200ps, '1' after 300ps;
-	R <= '0', '0' after 100ps, '1' after 200ps, '1' after 300ps;
+	--S <= '0', '1' after 100ps, '0' after 200ps, '1' after 300ps;
+	--R <= '0', '0' after 100ps, '1' after 200ps, '1' after 300ps;
 end architecture sim;
