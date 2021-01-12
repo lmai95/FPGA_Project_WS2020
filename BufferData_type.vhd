58cd474fa192dd5c86ac7b00e278d6aca32abece
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2021-01-12 18:26:57 +0100

    Update BufferData_type.vhd

--- BufferData_type.vhd
+++ BufferData_type.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 package BufferData is
   type DataSample is record
-    acc_x, acc_y, acc_z : integer RANGE 0 to 65536;--ToDo: Eigentlich ist ein kleinere Wertbereich aussreichend
+    acc_x, acc_y, acc_z : integer RANGE 0 to 65535;--ToDo: Eigentlich ist ein kleinere Wertbereich aussreichend
   end record;
   type DataSampleBuffer is array (natural  RANGE <>) of DataSample;
 
