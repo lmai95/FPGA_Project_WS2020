2ab18cc981627268707f11c42046c1399f970a10
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2021-01-12 18:27:04 +0100

    Update DataCollector.vhd

--- DataCollector.vhd
+++ DataCollector.vhd
@@ -13,16 +13,16 @@ entity DataCollector is
     Clk   	: in std_logic;        --Taktsignal des DataCollector
 
     data_valid : in std_logic;  						--data valid des Sensor Kontroll-Modul
-    acc_x 		 : in integer RANGE 0 to 65536; 	--x-achse des Sensor Kontroll-Modul; in m^2
-    acc_y 		 : in integer RANGE 0 to 65536; 	--y-achse des Sensor Kontroll-Modul; in m^2
-    acc_z 		 : in integer RANGE 0 to 65536; 	--z-achse des Sensor Kontroll-Modul; in m^2
+    acc_x 		 : in integer RANGE 0 to 65535; 	--x-achse des Sensor Kontroll-Modul; in m^2
+    acc_y 		 : in integer RANGE 0 to 65535; 	--y-achse des Sensor Kontroll-Modul; in m^2
+    acc_z 		 : in integer RANGE 0 to 65535; 	--z-achse des Sensor Kontroll-Modul; in m^2
 
     FiFoEmpty : in std_logic;			--FiFo ist leer
 	 FiFoFull : in std_logic;			--FiFo ist voll
 	 FiFoWrreq : out std_logic;		--FiFo Schreibanforderung
     FiFoData : out STD_LOGIC_VECTOR (47 DOWNTO 0) := (others => '0'); --Eingangs Daten der FiFo (acc_x, acc_y, acc_z)
     PrintRejectedData : out std_logic := '0';			--Bei '1': Die Anzahl der nicht verarbeitbaren Messungen (RejectedData) soll ausgaben werden
-	 RejectedData : out integer RANGE 0 to 65536 := 0	--Zaehler fuer die Anzahl der nicht verarbeitbaren Messungen waehrend die Daten geasammelt wurden
+	 RejectedData : out integer RANGE 0 to 65535 := 0	--Zaehler fuer die Anzahl der nicht verarbeitbaren Messungen waehrend die Daten geasammelt wurden
   );
 end entity DataCollector;
 
@@ -31,8 +31,8 @@ architecture behave of DataCollector is
   signal Step : integer RANGE 0 to 6 := 0;      				--aktueller Zustand der FSM
   signal NextStep : integer RANGE 0 to 6 := 0;  				--naechster Zustand der FSM
   
-  signal CntRejectedData : integer RANGE 0 to 65536 := 0;	--Anzahl der Datensaetze die Verworfen werden mussten
-  signal iCntRejectedData : integer RANGE 0 to 65536 := 0;
+  signal CntRejectedData : integer RANGE 0 to 65535 := 0;	--Anzahl der Datensaetze die Verworfen werden mussten
+  signal iCntRejectedData : integer RANGE 0 to 65535 := 0;
 BEGIN
   --Sammelt die Eingangswerte und legt sie in der FiFo in der Reihenfolge: acc_x, acc_y, acc_z ab.
   --Wenn es dazu kommt dass neue Eingangswerte vorhanden sind aber kein Platz in der FiFo verfuegbar ist werden die Daten verworfen.
@@ -57,13 +57,13 @@ BEGIN
 			WHEN 1 =>
 				--Einfuegen der Daten in FiFo
 				FiFoWrreq <= '1';
-				FiFoData <= (std_logic_vector(to_signed(acc_x, 16))) & (std_logic_vector(to_signed(acc_y, 16))) & std_logic_vector(to_signed(acc_z, 16));
+				FiFoData <= (std_logic_vector(to_unsigned(acc_x, 16))) & (std_logic_vector(to_unsigned(acc_y, 16))) & std_logic_vector(to_unsigned(acc_z, 16));
 				PrintRejectedData <= '0';
 				iCntRejectedData <= 0;
 			WHEN 2 =>
 				--Wartet auf data_valid  '0'
 				FiFoWrreq <= '0';
-				FiFoData <= (std_logic_vector(to_signed(acc_x, 16))) & (std_logic_vector(to_signed(acc_y, 16))) & std_logic_vector(to_signed(acc_z, 16));
+				FiFoData <= (std_logic_vector(to_unsigned(acc_x, 16))) & (std_logic_vector(to_unsigned(acc_y, 16))) & std_logic_vector(to_unsigned(acc_z, 16));
 				PrintRejectedData <= '0';
 				iCntRejectedData <= CntRejectedData;
 			WHEN 3 =>
