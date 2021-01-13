5e39e43c49404a57f34294dab12f62ab891e0970
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2021-01-13 14:51:39 +0100

    Update AsciiPrinter.vhd

--- AsciiPrinter.vhd
+++ AsciiPrinter.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.BufferData.all;
 
 entity AsciiPrinter is
 	generic(
@@ -15,9 +14,9 @@ entity AsciiPrinter is
 		Clk   	: in std_logic;
 
 		data_valid : in std_logic;						--data valid des Sensor Kontroll-Modul
-		acc_x 		 : in integer RANGE 0 to 65535; 	--x-achse des Sensor Kontroll-Modul; in m^2
-		acc_y 		 : in integer RANGE 0 to 65535; 	--y-achse des Sensor Kontroll-Modul; in m^2
-		acc_z 		 : in integer RANGE 0 to 65535; 	--z-achse des Sensor Kontroll-Modul; in m^2
+		acc_x 		 : in integer RANGE -32768 to 32767; 	--x-achse des Sensor Kontroll-Modul; in m^2
+		acc_y 		 : in integer RANGE -32768 to 32767; 	--y-achse des Sensor Kontroll-Modul; in m^2		7FFF = 32767 
+		acc_z 		 : in integer RANGE -32768 to 32767; 	--z-achse des Sensor Kontroll-Modul; in m^2
 		
 		TX_BUSY 	 : in std_logic;                            --TX_Busy der UART
 		TX_EN 		 : out std_logic := '0';                 --TX_EN der UART
