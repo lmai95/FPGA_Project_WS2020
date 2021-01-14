f15feac09ee1886ecb51a3f41813f2d5a5408a1a
Lukas Maier <lukas_maier@mailbox.org>
2021-01-14 15:18:18 +0100

    moved timings into the bitmaps.vhd

--- VGA.vhd
+++ VGA.vhd
@@ -1,118 +1,109 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
-use ieee.numeric_std.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+USE ieee.numeric_std.ALL;
+USE work.bitmaps.ALL;
 
-entity VGA is
-	port(
-		clk	: in std_logic;
-		rst	: in std_logic;
-		hsync : out std_logic;
-		vsync : out std_logic;
-		
-		screen_on : out std_logic;
-		hori_pos : out integer;
-		vert_pos : out integer
+ENTITY VGA IS
+	PORT
+	(
+		clk   : IN STD_LOGIC;
+		rst   : IN STD_LOGIC;
+		hsync : OUT STD_LOGIC;
+		vsync : OUT STD_LOGIC;
+
+		screen_on : OUT STD_LOGIC;
+		hori_pos  : OUT INTEGER;
+		vert_pos  : OUT INTEGER
 	);
-	
-end entity VGA;
 
-architecture behave of VGA is
-	
-	-- VGA Timing Werte aus TinyVGA.com
-	constant HD		: integer := 639;	-- Visible Area
-	constant HFP	: integer := 16;	-- Front Porch
-	constant HSP	: integer := 96;	-- Sync Pulse
-	constant HBP	: integer := 48;	-- Back Porch
-	
-	constant VD		: integer := 479;	-- Visible Area 
-	constant VFP	: integer := 10;	-- Front Porch
-	constant VSP	: integer := 2;	-- Sync Pulse
-	constant VBP	: integer := 33;	-- Back Porch
-	
-	signal hpos		: integer := 0;	-- Momentane horizontal Position
-	signal vpos 	: integer := 0;	-- Momentane vertikale Postition
-	
-	signal on_screen : std_logic := '0';
+END ENTITY VGA;
 
-begin
-	
+ARCHITECTURE behave OF VGA IS
+
+	SIGNAL hpos : INTEGER := 0; -- Momentane horizontal Position
+	SIGNAL vpos : INTEGER := 0; -- Momentane vertikale Postition
+
+	SIGNAL on_screen : STD_LOGIC := '0';
+
+BEGIN
+
 	-- Horzontaler Zähler von links nach rechts
-	HPOS_counter : process(clk, rst)
-		begin
-			if rst = '0' then
+	HPOS_counter : PROCESS (clk, rst)
+	BEGIN
+		IF rst = '0' THEN
+			hpos <= 0;
+		ELSIF rising_edge(clk) THEN
+			IF (hpos = (HD + HFP + HSP + HBP)) THEN
 				hpos <= 0;
-			elsif rising_edge(clk) then
-				if(hpos = (HD + HFP + HSP + HBP)) then
-					hpos <= 0;
-				else
-					hpos <= hpos + 1;
-				end if;
-			end if;
-		end process;
-	
+			ELSE
+				hpos <= hpos + 1;
+			END IF;
+		END IF;
+	END PROCESS;
+
 	-- Vertikaler Zähler von oben nach unten
-	VPOS_counter : process(clk, rst, hpos)
-		begin
-			if rst = '0' then
-				vpos <= 0;
-			elsif rising_edge(clk) then
-				if(hpos = (HD + HFP + HSP + HBP)) then
-					if(vpos = (VD + VFP + VSP + VBP)) then
-						vpos <= 0;
-					else
-						vpos <= vpos + 1;
-					end if;
-				end if;
-			end if;
-		end process;
-	
+	VPOS_counter : PROCESS (clk, rst, hpos)
+	BEGIN
+		IF rst = '0' THEN
+			vpos <= 0;
+		ELSIF rising_edge(clk) THEN
+			IF (hpos = (HD + HFP + HSP + HBP)) THEN
+				IF (vpos = (VD + VFP + VSP + VBP)) THEN
+					vpos <= 0;
+				ELSE
+					vpos <= vpos + 1;
+				END IF;
+			END IF;
+		END IF;
+	END PROCESS;
+
 	-- Horizontal Synchrosignal 
-	H_Synchro : process(clk, rst, hpos)
-		begin
-			if rst = '0' then
+	H_Synchro : PROCESS (clk, rst, hpos)
+	BEGIN
+		IF rst = '0' THEN
+			hsync <= '0';
+		ELSIF rising_edge(clk) THEN
+			IF ((hpos <= (HD + HFP)) OR (hpos > HD + HFP + HSP)) THEN
+				hsync <= '1';
+			ELSE
 				hsync <= '0';
-			elsif rising_edge(clk) then
-				if((hpos <= (HD + HFP)) or (hpos > HD + HFP + HSP)) then
-					hsync <= '1';
-				else
-					hsync <= '0';
-				end if;
-			end if;
-		end process;
-			
+			END IF;
+		END IF;
+	END PROCESS;
+
 	-- Vertikal Synchrosignal
-	V_Synchro : process(clk, rst, vpos)
-		begin
-			if rst = '0' then
+	V_Synchro : PROCESS (clk, rst, vpos)
+	BEGIN
+		IF rst = '0' THEN
+			vsync <= '0';
+		ELSIF rising_edge(clk) THEN
+			IF ((vpos <= (VD + VFP)) OR (vpos > VD + VFP + VSP)) THEN
+				vsync <= '1';
+			ELSE
 				vsync <= '0';
-			elsif rising_edge(clk) then
-				if((vpos <= (VD + VFP)) or (vpos > VD + VFP + VSP)) then
-					vsync <= '1';
-				else
-					vsync <= '0';
-				end if;
-			end if;
-		end process;
-	
+			END IF;
+		END IF;
+	END PROCESS;
+
 	-- 1 == Zähler ist im Bildschirm ==> Darstellung OK!
 	-- 0 == Zähler ist nicht im sichtbaren Teil ==> Darstellung NG!
-	on_screen_check : process(clk, rst, hpos, vpos)
-		begin
-			if rst = '0' then
+	on_screen_check : PROCESS (clk, rst, hpos, vpos)
+	BEGIN
+		IF rst = '0' THEN
+			on_screen <= '0';
+		ELSIF rising_edge(clk) THEN
+			IF (hpos <= HD AND vpos <= VD) THEN
+				on_screen <= '1';
+			ELSE
 				on_screen <= '0';
-			elsif rising_edge(clk) then
-				if(hpos <= HD and vpos <= VD) then
-					on_screen <= '1';
-				else
-					on_screen <= '0';
-				end if;
-			end if;
-		end process;
-	
+			END IF;
+		END IF;
+	END PROCESS;
+
 	-- Signale "abfangen" um sie außerhalb dieses Moduls zu verwenden
 	hori_pos <= hpos;
 	vert_pos <= vpos;
 	screen_on <= on_screen;
-	
-end architecture behave;
+
+END ARCHITECTURE behave;
