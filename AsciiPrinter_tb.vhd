e3bdf5ee26d5c9b28e15ffe120a2b74c242635a4
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2021-01-20 17:10:57 +0100

    Update AsciiPrinter_tb.vhd

--- AsciiPrinter_tb.vhd
+++ AsciiPrinter_tb.vhd
@@ -16,6 +16,10 @@ architecture behave of AsciiPrinter_tb is
   signal TX_BUSY : std_logic;
   signal TX_EN : std_logic;
   signal TX_DATA : std_logic_vector(7 downto 0);
+  signal data_valid_in : std_logic := '0';	              --Ausgabe von data valid an das VGA-Modl
+  signal x_in : integer range -9999 to 9999 := 0;        --Ausgabe der Beschleunigung der x-achse in cm/s^2 an das VGA-Modul
+  signal	y_in : integer range -9999 to 9999 := 0;        --Ausgabe der Beschleunigung der y-achse in cm/s^2 an das VGA-Modul
+  signal	z_in : integer range -9999 to 9999 := 0;         --Ausgabe de
 BEGIN
   Tester: entity work.AsciiPrinter_tester(test)
     port map(
@@ -31,7 +35,7 @@ BEGIN
       TX_DATA => TX_DATA
     );
 
-  DUT: entity work.AsciiPrinter(behave)
+  DUT: entity work.signal_processing(behave)
     generic map(
       FreeRunning => '0'
     )
@@ -45,6 +49,10 @@ BEGIN
     acc_z => acc_z,
     TX_BUSY => TX_BUSY,
     TX_EN => TX_EN,
-    TX_DATA => TX_DATA
+    TX_DATA => TX_DATA,
+	 data_valid_in => data_valid_in,
+	 x_in => x_in,										
+	 y_in => y_in,									
+	 z_in => z_in	
     );
 end architecture behave;
