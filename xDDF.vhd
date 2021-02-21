a3ab16783cfc4fb0a6f90eafc2a3ea3bd5bbc298
tobiasruber <75737609+tobiasruber@users.noreply.github.com>
2021-02-21 14:09:08 +0100

    Update xDDF.vhd

--- xDDF.vhd
+++ xDDF.vhd
@@ -1,13 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+--------------------------------------------------------------------
+--	DDF bildet ein D-FlipFlop ab
+-- die beiden Ausgänge sind mit einer Delay-Zeit versehen
+--------------------------------------------------------------------
 entity xDDF is
 	generic(
 		TCO 	: time := 10ns;
 		TD		: time := 6ns
 	);
 	port(
-		Reset	: in std_logic := '0';
+		Reset	: in std_logic := '0';		--Eingänge für Simulation mit sinnvollen Werten initialisieren
 		Set	: in std_logic := '0';
 		sClear: in std_logic := '0';
 		D		: in std_logic;
@@ -19,16 +23,16 @@ entity xDDF is
 end entity xDDF;
 
 architecture behave of xDDF is
-	signal Qi : std_logic;
+	signal Qi : std_logic;					--internes Ausgangssignal
 begin
 
 clocked: process(Reset,Set ,Clk)
 begin
-		if (Reset = '1' ) then
+		if (Reset = '1' ) then				--Resetfall: Ausgang reseten
 			Qi <= '0';
-		elsif (Set = '1') then
+		elsif (Set = '1') then				--Setfall: Ausgang setzten
 			Qi <= '1';
-		elsif (rising_edge(Clk))then
+		elsif (rising_edge(Clk))then		--bei steigender Flanke Daten am Ausgang übernehmen oder löschen
 			if sClear ='1'then
 				Qi <= '0';
 			elsif EN  ='1' then
@@ -37,7 +41,7 @@ begin
 		end if;
 end process clocked;
 
-Q <= Qi after TCO;
-Qn <= not Qi after TCO + TD;
+Q <= Qi after TCO;							--Delay des Ausgangs, hier 10ns
+Qn <= not Qi after TCO + TD;				--Delay des invertierten-Ausgangs nach 16ns
 
 end architecture behave;
